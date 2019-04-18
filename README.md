174e91c1f77b38ca04806eb799982216ea774137
Daniel Gordi <danial@gordi.ir>
2019-04-18 22:11:54 +0430

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # imapsync-WebUI
 A WebUI with simple Go app to sync emails with ImapSync
+
+## Features:
+- Sync email account between two IMAP Server
+- Supports latest encryption methods (legacy imapcopy won't)
+- Fast as f*ck
---
+++ imapsync-web.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os/exec"
+    "strings"
+
+)
+
+
+func hello(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    switch r.Method {
+    case "GET":
+         http.ServeFile(w, r, "form.html")
+    case "POST":
+        // Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+        if err := r.ParseForm(); err != nil {
+            fmt.Fprintf(w, "ParseForm() err: %v", err)
+            return
+        }
+	fmt.Fprintf(w, "Process should began, Please wait for 10 minutes")
+        name := r.FormValue("email")
+        oldpass := r.FormValue("oldpass")
+	newpass := r.FormValue("newpass")
+	cmdName := ("sudo /usr/bin/imapsync --host1 mail.cafebazaar.ir --user1 " + name + " --password1 " + oldpass + " --host2 127.0.0.1 --user2 " + name + " --password2 " + newpass)
+	cmdArgs := strings.Fields(cmdName)
+	cmd	:= exec.Command(cmdArgs[0], cmdArgs[1:len(cmdArgs)]...)
+	cmd.Start()
+
+    default:
+        fmt.Fprintf(w, "Sorry, only GET and POST methods are supported.")
+    }
+}
+
+func main() {
+    http.HandleFunc("/", hello)
+    fmt.Printf("Starting server for testing HTTP POST...\n")
+
+    if err := http.ListenAndServe(":666", nil); err != nil {
+        log.Fatal(err)
+    }
+}
+
+
